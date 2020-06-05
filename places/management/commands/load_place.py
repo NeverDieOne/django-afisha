36d8f5b6205079ac027f42dbbe7d106f96516fde
neverdie <altiore@mail.ru>
2020-06-06 02:04:41 +0300

    Add load_place command to download json with place and add it to base

---
+++ places/management/commands/load_place.py
@@ -0,0 +1,45 @@
+from django.core.management.base import BaseCommand
+import requests
+from places.models import Place, PlaceImage
+from django.core.files.base import ContentFile
+
+
+class Command(BaseCommand):
+    help = 'Add place to map by json object'
+
+    def add_arguments(self, parser):
+        parser.add_argument('url', help='URL to download json file with place')
+
+    def handle(self, *args, **options):
+        url = options['url']
+        response = requests.get(url)
+        response.raise_for_status()
+
+        place_json = response.json()
+        title = place_json['title']
+        imgs = place_json['imgs']
+        desc_short = place_json['description_short']
+        desc_long = place_json['description_long']
+        lng, lat = place_json['coordinates'].values()
+
+        place, _ = Place.objects.get_or_create(
+            title=title,
+            description_short=desc_short,
+            description_long=desc_long,
+            lat=lat,
+            lng=lng
+        )
+
+        current_pos = 1
+        for img in imgs:
+            response = requests.get(img)
+            response.raise_for_status()
+
+            image_content = ContentFile(response.content)
+            place_image_obj = PlaceImage.objects.create(
+                place=place,
+                position=current_pos
+            )
+
+            place_image_obj.image.save(f'{title}-{current_pos}.jpg', image_content, save=True)
+            current_pos += 1
