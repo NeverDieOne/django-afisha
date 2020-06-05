d9970f51584dd25984d1df83ae997a77f71d9091
neverdie <altiore@mail.ru>
2020-06-05 23:35:53 +0300

    Add admin-sotrable-2

--- places/admin.py
+++ places/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
-
+from adminsortable2.admin import SortableInlineAdminMixin
 from .models import Place, PlaceImage
 
 
-class ImageInline(admin.TabularInline):
+class ImageInline(SortableInlineAdminMixin, admin.TabularInline):
     model = PlaceImage
     readonly_fields = ["place_image", ]
     fields = ('image', 'place_image', 'position')
+    extra = 0
 
     def place_image(self, obj):
         return mark_safe('<img src="{url}" height={height} />'.format(
