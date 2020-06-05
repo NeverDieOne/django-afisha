334a7adab1dd1da98c2d657da56d37d30205fbee
neverdie <altiore@mail.ru>
2020-06-05 23:46:49 +0300

    Add tinymce

--- afisha/urls.py
+++ afisha/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('tinymce/', include('tinymce.urls')),
     path('', include('places.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- places/models.py
+++ places/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 
 class Place(models.Model):
     title = models.CharField('Название', max_length=250, db_index=True)
     description_short = models.TextField('Короткое описание', db_index=True)
-    description_long = models.TextField('Длинное описание', db_index=True)
+    description_long = HTMLField('Длинное описание', db_index=True)
     lat = models.CharField('Широта', max_length=50, db_index=True)
     lng = models.CharField('Долгота', max_length=50, db_index=True)
 
