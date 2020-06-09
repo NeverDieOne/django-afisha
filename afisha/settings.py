41a7eced9157dd09adc830d5cc527147058d16c7
neverdie <altiore@mail.ru>
2020-06-10 00:09:50 +0300

    Change templates dir for places app

--- afisha/settings.py
+++ afisha/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'afisha.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'places/templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
